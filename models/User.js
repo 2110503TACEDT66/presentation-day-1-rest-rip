a5f8c6d130ecbf5bb44f700644525887843f20ae
Saksorn Yoothong-in <sasipim890@gmail.com>
2024-03-02 13:26:03 +0700

    fix role

--- models/User.js
+++ models/User.js
@@ -18,7 +18,7 @@ const UserSchema =  new mongoose.Schema({
     },
     role : {
         type : String,
-        enum : ['user','admin','publisher'],
+        enum : ['user','admin'],
         default : 'user'
     },
     password : {
