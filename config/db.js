54dc4ad8367fe02bab86e895d84c9ac026562f50
PeemSx <peemp5802@gmail.com>
2024-03-25 19:39:42 +0700

    connection string

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect("mongodb+srv://ladeeda:1234@co-work-space.p4wrvcn.mongodb.net/?retryWrites=true&w=majority");
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
