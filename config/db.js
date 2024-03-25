2244f34b3389e859fa35a4d3677a9ee694962982
PeemSx <peemp5802@gmail.com>
2024-03-25 19:34:24 +0700

    error handling

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    mongoose.set('strictQuery',true);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-
+    try {
+        mongoose.set('strictQuery', true);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        // Handle error (e.g., throw an error, exit the process)
+        process.exit(1);
+    }
 }
 
 module.exports = connectDB;
